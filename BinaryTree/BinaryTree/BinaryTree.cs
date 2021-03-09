1aa8837ec2c80d03ab3fbedbaee1867b4fb317cb
Laurent Marzullo <laurent.marzullo@gmail.com>
2021-03-09 11:19:02 +0100

    [BST] Insert

--- BinaryTree/BinaryTree/BinaryTree.cs
+++ BinaryTree/BinaryTree/BinaryTree.cs
@@ -2,25 +2,64 @@
 
 namespace BinaryTree
 {
-    enum Color
-    {
-        Red,
-        Black
-    }
-
-
-    public class BinaryTree<T>
+    public class BinaryTree<T> where T: IComparable
     {
         public class Node
         {
-            T   value;
+            private enum ChildDirection
+            {
+                Left = 0,
+                Right
+            }
+
+            public T value { get; set; }
+
             Node[] children = new Node[2];
-            Node parent;
-            Color color = Color.Red;
+
+            public Node(T value)
+            {
+                this.value = value;
+            }
+
+            public Node left() { return children[(int)ChildDirection.Left];}
+            public void left(Node left_child)
+            {
+                children[(int)ChildDirection.Left] = left_child;
+            }
+            public Node right() { return children[(int)ChildDirection.Right];}
+            public void right(Node right_child)
+            {
+                children[(int)ChildDirection.Right] = right_child;
+            }
         }
+
+
 
         public Node getRoot() { return _root; }
 
+        public void insert(T value)
+        {
+            var new_node = new Node(value);
+            _root = BSTInsert(_root, new_node);
+        }
+
         private Node _root;
+
+
+        private Node BSTInsert(Node _root, Node new_node)
+        {
+            if (_root == null)
+                return new_node;
+
+            if (new_node.value.CompareTo(_root.value) < 0)
+            {
+                _root.left(BSTInsert(_root.left(), new_node));
+            }
+            else if (new_node.value.CompareTo(_root.value) > 0)
+            {
+                _root.right(BSTInsert(_root.right(), new_node));
+            }
+            return _root;
+        }
     }
 }
--- BinaryTree/BinaryTreeTest/BinaryTreeTest.cs
+++ BinaryTree/BinaryTreeTest/BinaryTreeTest.cs
@@ -6,16 +6,73 @@ namespace BinaryTreeTest
     [TestFixture]
     public class BinaryTreeTest
     {
+        private BinaryTree<int> tree;
+
+
         [SetUp]
         public void Setup()
         {
+            tree = new BinaryTree<int>();
         }
 
         [Test]
         public void CreatingEmptyBinaryTree()
         {
-            var tree = new BinaryTree<string>();
-            Assert.AreEqual(tree.getRoot(), null);
+            Assert.AreEqual(null, tree.getRoot());
+        }
+
+
+        [Test]
+        public void InsertFirstValue()
+        {
+            tree.insert(1000);
+
+            Assert.AreNotEqual(null, tree.getRoot());
+            Assert.AreEqual(1000, tree.getRoot().value);
+        }
+
+
+        [Test]
+        public void BuildBSTree()
+        {
+            tree.insert(1000);
+            tree.insert(500);
+            tree.insert(1500);
+            tree.insert(2000);
+            tree.insert(1);
+            tree.insert(3000);
+            tree.insert(1250);
+            //
+            //          1000
+            //          /  \
+            //       500   1500
+            //       /     /  \
+            //     1    1250  2000
+            //                  \
+            //                  3000
+
+            Assert.AreEqual(1000, tree.getRoot().value);
+
+            Assert.AreNotEqual(null, tree.getRoot().left().value);
+            Assert.AreEqual(500, tree.getRoot().left().value);
+            Assert.AreEqual(null, tree.getRoot().left().right());
+
+            Assert.AreNotEqual(null, tree.getRoot().left().left());
+            Assert.AreEqual(1, tree.getRoot().left().left().value);
+            Assert.AreEqual(null, tree.getRoot().left().left().left());
+            Assert.AreEqual(null, tree.getRoot().left().left().right());
+
+            Assert.AreNotEqual(null, tree.getRoot().right());
+            Assert.AreEqual(1500, tree.getRoot().right().value);
+
+            Assert.AreNotEqual(null, tree.getRoot().right().left());
+            Assert.AreEqual(1250, tree.getRoot().right().left().value);
+
+            Assert.AreNotEqual(null, tree.getRoot().right().right());
+            Assert.AreEqual(2000, tree.getRoot().right().right().value);
+
+            Assert.AreNotEqual(null, tree.getRoot().right().right().right());
+            Assert.AreEqual(3000, tree.getRoot().right().right().right().value);
         }
     }
 }
